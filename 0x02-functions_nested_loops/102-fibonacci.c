8a914f55ffbd56db4422e6cd757f3d98c0f6b87f
oluwatobiju <oluwatobijuakinla05@gmail.com>
2022-10-18 18:32:42 -0700

    Task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,21 +8,21 @@
  */
 int main(void)
 {
-	long int i, j, k, next;
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
 
-	j = 1;
-
-	k = 2;
-
-	for (i = 1; i <= 50; ++i)
+	for (count = 0; count < 50; count++)
 	{
-		if (j != 20365011074)
-		{
-			printf("%ld, ", j);
-		}
-		next = j + k;
-		j = k;
-		k = next;
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
 	}
 	return (0);
 }
